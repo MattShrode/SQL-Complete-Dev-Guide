81b558f450816803a1ba7e1cd17173805bfe7d40
Matt Shrode <mattshrode@gmail.com>
2023-01-22 14:44:44 -0600

    Section 4 - Delete Tables
    Delete the conversations and employers tables and recreate them
    using the commands with default values.

---
+++ Section-4-Data-Definition/10-Insert-Data-Again.sql
@@ -0,0 +1,11 @@
+INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+VALUES ('Test Co.', '123 Main Street', 5.5, true);
+
+INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+VALUES ('Learning Co.', '456 First Street', 0.55, true);
+
+INSERT INTO employers (company_name, company_address, yearly_revenue) 
+VALUES ('Widget Inc.', '789 Second Street', 10.5);
+
+INSERT INTO conversations (user_name, employer_name, message) 
+VALUES ('Matt Shrode', 'Learning Co.', 'Hi, I would like to apply to your company!');
