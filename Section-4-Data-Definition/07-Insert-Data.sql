3dace4b3d311e3a95e0ed740ff0dda95499a89f4
Matt Shrode <mattshrode@gmail.com>
2023-01-22 11:19:52 -0600

    Section 4 - Insert Into
    Insert data into the employers and conversations tables.

---
+++ Section-4-Data-Definition/07-Insert-Data.sql
@@ -0,0 +1,11 @@
+/* INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+VALUES ('Test Co.', '123 Main Street', 5.5, true);
+
+INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+VALUES ('Learning Co.', '456 First Street', 0.55, true);
+
+INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+VALUES ('Widget Inc.', '789 Second Street', 10.5, false); */
+
+INSERT INTO conversations (user_name, employer_name, message, date_sent) 
+VALUES ('Matt Shrode', 'Learning Co.', 'Hi, I would like to apply to your company!', '2023-2-5 16:32:10');
