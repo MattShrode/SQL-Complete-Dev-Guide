5fec13d1fd8138ee4066c309dc5722b4292ef206
Matt Shrode <mattshrode@gmail.com>
2023-01-23 20:59:20 -0600

    Section 5 - Date and Range Filtering
    Create commands that filter dates and ranges.

---
+++ Section-5-Data-Manipulation/09-Date-and-Range-Filtering.sql
@@ -0,0 +1,10 @@
+-- 4. Find all sales between two dates
+-- SELECT * FROM sales
+-- WHERE (date_created > '2021-11-01') AND (date_created < '2022-05-01');
+
+-- SELECT * FROM sales
+-- WHERE date_created BETWEEN '2021-11-01' AND '2022-05-01';
+-- Between is inclusive!
+
+SELECT * FROM sales
+WHERE volume BETWEEN 1000 AND 100000;
