2cda243fb3118342e42dab7249076daaf68d031c
Matt Shrode <mattshrode@gmail.com>
2023-01-23 20:34:35 -0600

    Section 5 - Select Column Options
    Learn about options for modifying the columns displayed in the
    result set.

--- Section-5-Data-Manipulation/07-Select-Data.sql
+++ Section-5-Data-Manipulation/07-Select-Data.sql
@@ -2,4 +2,13 @@
 -- SELECT * FROM sales;
 
 -- Specific columns
-SELECT customer_name, product_name, volume, date_created FROM sales;
+-- SELECT customer_name, product_name, volume, date_created FROM sales;
+
+-- Renaming columns in the result set.
+-- SELECT customer_name, product_name, volume AS total_sales, date_created FROM sales;
+
+-- Adding hard coded values into result sets
+-- SELECT 'Hello World', customer_name, product_name, volume, date_created FROM sales;
+
+-- Perform calculations on data in the result set
+SELECT customer_name, product_name, volume / 1000 AS total_sales, date_created FROM sales;
