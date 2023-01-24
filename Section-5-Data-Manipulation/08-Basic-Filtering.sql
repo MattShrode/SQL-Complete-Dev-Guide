b4e0dffae2527d99066f76b0990692f5a4063b0a
Matt Shrode <mattshrode@gmail.com>
2023-01-23 20:50:29 -0600

    Section 5 - Basic Filtering
    Create commands with basic filters.

---
+++ Section-5-Data-Manipulation/08-Basic-Filtering.sql
@@ -0,0 +1,11 @@
+-- 1. Find all sales greater than 1000
+-- SELECT * FROM sales
+-- WHERE volume > 1000;
+
+-- 2. Find all recurring sales
+-- SELECT * FROM sales
+-- WHERE is_recurring IS TRUE;
+
+-- 3. Find disputed sales with volume > 1000
+-- SELECT * FROM sales
+-- WHERE (is_disputed IS TRUE) AND (volume > 1000);
