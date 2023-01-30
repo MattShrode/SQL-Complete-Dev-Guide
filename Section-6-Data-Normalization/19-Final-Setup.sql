d79e5eec13751a2e16c6ceec83a69cd8ce672492
Matt Shrode <mattshrode@gmail.com>
2023-01-30 16:01:11 -0600

    Section 6 - Practice Deletions
    Practice delete data and see how the foreign key constraints respond.

--- Section-6-Data-Normalization/19-Final-Setup.sql
+++ Section-6-Data-Normalization/19-Final-Setup.sql
@@ -1,26 +1,26 @@
 CREATE TABLE projects (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
-    -- id SERIAL PRIMARY KEY, -- Postgres
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- Postgres
     title VARCHAR(300) NOT NULL,
     deadline DATE
 );
 
 CREATE TABLE company_buildings (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
-    -- id SERIAL PRIMARY KEY, -- Postgres
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- Postgres
     name VARCHAR(300) NOT NULL
 );
 
 CREATE TABLE teams (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
-    -- id SERIAL PRIMARY KEY, -- Postgres
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- Postgres
     name VARCHAR(300) NOT NULL,
     building_id INT REFERENCES company_buildings (id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
-    -- id SERIAL PRIMARY KEY, -- Postgres
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- Postgres
     first_name VARCHAR(300) NOT NULL,
     last_name VARCHAR(300) NOT NULL,
     birthdate DATE NOT NULL,
@@ -30,16 +30,16 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE intranet_accounts (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
-    -- id SERIAL PRIMARY KEY, -- Postgres
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- Postgres
     email VARCHAR(200) REFERENCES employees (email) ON DELETE CASCADE,
     password VARCHAR(200) NOT NULL
 );
 
 -- Intermediate Table => n:n
 CREATE TABLE projects_employees (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
-    -- id SERIAL PRIMARY KEY, -- Postgres
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- Postgres
     employee_id INT REFERENCES employees (id) ON DELETE CASCADE,
     project_id INT REFERENCES projects (id) ON DELETE CASCADE
 );
