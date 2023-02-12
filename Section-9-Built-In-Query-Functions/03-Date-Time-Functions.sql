4d8a376d1be05881fba2842447280b4179a73f85
Matt Shrode <mattshrode@gmail.com>
2023-02-12 15:02:12 -0600

    Section 9 - Built In Queries - Weekdays
    Practice extracting weekdays and splitting dates and times.

--- Section-9-Built-In-Query-Functions/03-Date-Time-Functions.sql
+++ Section-9-Built-In-Query-Functions/03-Date-Time-Functions.sql
@@ -1,2 +1,18 @@
-SELECT EXTRACT(DAY FROM last_checkin) AS month, last_checkin
+-- SELECT EXTRACT(DAY FROM last_checkin) AS month, last_checkin
+-- FROM memberships;
+
+-- Extracts day of the week in Postgres, Sunday starts with 0
+-- SELECT EXTRACT(DOW FROM last_checkin) AS month, last_checkin
+-- FROM memberships;
+
+-- Extract day of the week in MySQL, Monday starts with 0
+-- SELECT WEEKDAY(last_checkin), last_checkin
+-- FROM memberships;
+
+-- Split Date and Time in MySQL
+-- SELECT CONVERT(last_checkin, DATE), CONVERT(last_checkin, TIME)
+-- FROM memberships;
+
+-- Split Date and Time in Postgres
+SELECT last_checkin::TIMESTAMP::DATE, last_checkin::TIMESTAMP::TIME
 FROM memberships;
