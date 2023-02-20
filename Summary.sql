43e5371a50d487820877ba9e4af23382f97e2b13
Matt Shrode <mattshrode@gmail.com>
2023-02-19 22:42:20 -0600

    Summary - General Syntax
    Document the general SQL syntax.

---
+++ Summary.sql
@@ -0,0 +1,12 @@
+-- General Syntax Example Command:
+SELECT name, salary FROM employees WHERE salary > 8000;
+
+-- Key Words: SELECT, FROM, WHERE
+-- Identifiers - Colummns: name, salary
+-- Identifiers - Table: employees
+-- Clauses:
+    -- 1. SELECT name, salary
+    -- 2. FROM employees
+    -- 3. WHERE Salary > 8000
+-- Value/Expression: 8000
+-- Operator: >
